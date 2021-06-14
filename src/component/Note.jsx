a6ee08c6f5a44952f5673e771f3c4e4af6558796
Armagan Dalkiran <armagandalkran20@gmail.com>
2021-06-14 21:41:00 +0300

    14.06.2021 First Commit

---
+++ src/component/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>My Title</h1>
+      <p>My Paragraph is going to be like this.</p>
+    </div>
+  );
+}
+
+export default Note;
